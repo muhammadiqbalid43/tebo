a990a3a2d88a887e57ce5d395bfa8dc13b4c1667
Muhammad Iqbal <muhammadiqbalid43@gmail.com>
2024-11-14 16:54:16 +0700

    feat: add editor post

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,7 +1,11 @@
+import PostEditor from "@/components/posts/editor/post-editor";
+
 export default function Home() {
   return (
-    <main className="h-[200vh] w-full bg-red-50">
-      <div>Front page</div>
+    <main className="h-[200vh] w-full ">
+      <div className="w-full">
+        <PostEditor />
+      </div>
     </main>
   );
 }
---
+++ components/posts/editor/post-editor.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import { EditorContent, useEditor } from "@tiptap/react";
+import Placeholder from "@tiptap/extension-placeholder";
+import StarterKit from "@tiptap/starter-kit";
+import { submitPost } from "./actions";
+import { Button } from "@/components/ui/button";
+import { useSession } from "@/app/(main)/session-provider";
+import UserAvatar from "@/components/user-avatar";
+import "./styles.css";
+
+export default function PostEditor() {
+  const { user } = useSession();
+  const editor = useEditor({
+    extensions: [
+      StarterKit.configure({
+        bold: false,
+        italic: false,
+      }),
+      Placeholder.configure({
+        placeholder: "What's crack-a-lackin'?",
+      }),
+    ],
+  });
+
+  const input =
+    editor?.getText({
+      blockSeparator: "\n",
+    }) || "";
+
+  async function onSubmit() {
+    await submitPost(input);
+    editor?.commands.clearContent();
+  }
+  return (
+    <div className="flex flex-col">
+      <div className="flex gap-5">
+        <UserAvatar avatarUrl={user.avatarUrl} className="hidden sm:inline" />
+        <EditorContent
+          editor={editor}
+          className="max-h-[20rem] w-full focus-visible:outline-none overflow-y-auto rounded-2xl bg-slate-100 text-slate-600 px-5 py-3"
+        />
+      </div>
+      <div className="flex justify-end">
+        <Button
+          onClick={onSubmit}
+          disabled={!input.trim()}
+          className="min-w-20"
+        >
+          Post
+        </Button>
+      </div>
+    </div>
+  );
+}
